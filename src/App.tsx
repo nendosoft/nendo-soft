adccdf790e8d70a36b86b50b5cb8e37fa90ff48d
nendosoft <nendosoft@gmail.com>
2021-02-15 18:34:51 +0900

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,11 @@
+import 'react-perfect-scrollbar/dist/css/styles.css';
 import React from 'react';
+import { useRoutes } from 'react-router-dom';
+import { ThemeProvider } from '@material-ui/core';
+// import GlobalStyles from 'src/components/GlobalStyles';
+// import 'src/mixins/chartjs';
+// import theme from 'src/theme';
+// import routes from 'src/routes';
 import logo from './logo.svg';
 import './App.css';
 
@@ -21,6 +28,14 @@ function App() {
       </header>
     </div>
   );
+  // const routing = useRoutes(routes);
+
+  // return (
+    // <ThemeProvider theme={theme}>
+    //   <GlobalStyles />
+    //   {routing}
+    // </ThemeProvider>
+  // );
 }
 
 export default App;
